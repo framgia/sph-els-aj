968e3fa28f0241eba9df51059f9a5b8121c3cf74
Abdul Jalil Palala <108660012+abduljalilpalala@users.noreply.github.com>
2022-07-26 12:05:20 +0800

    [BE] Create User API (#1)
    * Create User API
    
    * UPDATE: Renamed the Controllers and moved some functions
    
    * UPDATE: Added lines to web.php

---
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisteredUserRequest;
+use App\Services\Auth\RegisterUserService;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(RegisteredUserRequest $request)
+    {
+        $user = new RegisterUserService($request->validated());
+        $user->register();
+        // Auth::login($user);
+        return response()->noContent();
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return public_path('avatar\default.png');
+    // return ['Laravel' => app()->version()];
 });
+
+
+require __DIR__.'/auth.php';
