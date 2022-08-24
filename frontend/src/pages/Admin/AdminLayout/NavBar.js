5ee832351f26708bc769debad5040b5982aa736d
Abdul Jalil Palala <108660012+abduljalilpalala@users.noreply.github.com>
2022-08-24 10:52:28 +0800

    [FE] Add Loader when Logging out (#38)

--- frontend/src/pages/Admin/AdminLayout/NavBar.js
+++ frontend/src/pages/Admin/AdminLayout/NavBar.js
@@ -24,7 +24,6 @@ const DRAWER_WIDTH = 240;
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
--- frontend/src/pages/User/UserLayout/NavBar/UserNavItem.js
+++ frontend/src/pages/User/UserLayout/NavBar/UserNavItem.js
@@ -13,6 +13,7 @@ import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import { useAuth } from "../../../../hooks/auth";
+import BackdropLoader from "../../../../components/BackdropLoader";
 
 const settings = [
   { text: "Dashboard", path: "/dashboard" },
@@ -22,6 +23,7 @@ const settings = [
 
 const UserNavItem = () => {
   const { user, logout } = useAuth({ middleware: "auth" });
+  const [loading, setLoading] = useState(false);
 
   const [anchorElUser, setAnchorElUser] = useState(null);
 
@@ -33,8 +35,15 @@ const UserNavItem = () => {
     setAnchorElUser(null);
   };
 
+  const handleLogoutClick = () => {
+    setLoading(true);
+    handleCloseUserMenu();
+    logout();
+  };
+
   return (
     <Box sx={{ flexGrow: 0 }}>
+      <BackdropLoader loading={loading} />
       {!user ? (
         <Skeleton variant="circular" width={40} height={40} />
       ) : (
@@ -62,13 +71,20 @@ const UserNavItem = () => {
       >
         {settings.map(({ text, path }) => (
           <MenuItem key={text}>
-            <Link component={RouterLink} to={path} variant="body2">
+            <Link
+              component={RouterLink}
+              to={path}
+              variant="body2"
+              sx={{ textDecoration: "none" }}
+            >
               {text}
             </Link>
           </MenuItem>
         ))}
-        <MenuItem onClick={logout}>
-          <Typography textAlign="center">Logout</Typography>
+        <MenuItem onClick={() => handleLogoutClick()}>
+          <Typography textAlign="center" color="#1976d2" fontSize="0.875rem">
+            Logout
+          </Typography>
         </MenuItem>
       </Menu>
     </Box>
